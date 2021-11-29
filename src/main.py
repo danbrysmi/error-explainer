d2b475d97674752b0c94d47689e95cf7f97f4728
Dan Bryan-Smith <danbrysmi@gmail.com>
2021-11-29 15:03:50 +0000

    Added colon detection
    Added colon detection after keywords

--- src/main.py
+++ src/main.py
@@ -6,25 +6,33 @@ class Converter():
     """
 
     def __init__(self, file):
+        self.block_starters = ["if", "elif", "else", "while", "for"]
         f = open(f"data/{file}", "r")
         self.lines = f.readlines()
+
         print("Initial")
+        # removing "\n" from end of each line in text file
+        self.lines[:-1] = list(map(lambda x: x[:-1], self.lines[:-1]))
         for line in self.lines:
             print([line])
 
-        self.lines = list(map(lambda x: self.remove_comments(x), self.lines))
         print("Comments Removed...")
+        self.lines = list(map(lambda x: self.remove_comments(x), self.lines))
         for line in self.lines:
             print([line])
 
-        self.lines = list(filter(lambda x: self.filter_newlines(x), self.lines))
+        print("Checking Syntax...")
+        self.lines = list(map(lambda x: self.check_syntax(x), self.lines))
+        for line in self.lines:
+            print([line])
+
         print("Newlines and Deleted Lines Removed...")
+        self.lines = list(filter(lambda x: self.filter_newlines(x), self.lines))
         for line in self.lines:
             print([line])
 
 
-
-
+    # TIDYING UP THE CODE
     def filter_newlines(self, line): # Can be called on a line
         return line != "\n" and line != ""
 
@@ -39,6 +47,7 @@ class Converter():
 
         return line.strip()[0] != "#"
 
+    # UTIL?
     def char_in_quotes(self, char_index, line):
         char_list = list(line)
         q1_count = 0
@@ -49,6 +58,22 @@ class Converter():
             if char_list[i] == '"':
                 q1_count += 1
         return (q1_count % 2) != 0
+
+    def missing_colon(self, line):
+        l = line.strip()
+        if len(l.split()) == 0:
+            return line
+        if l.split()[0] in self.block_starters:
+            if l[-1] != ":":
+                return line + "<:>"
+            else:
+                return line
+        else:
+            return line
+
+    def check_syntax(self, line):
+        line1 = self.missing_colon(line)
+        return line1
 
 converter = Converter("python.txt")
 
