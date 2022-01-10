98fa4cb583882e8802f1adeb9589c543153c6cf3
Dan Bryan-Smith <danbrysmi@gmail.com>
2022-01-10 14:21:53 +0000

    Fixed formatting of results page trace
    Also identified which exception class is in the traceback

--- myproject/results/views.py
+++ myproject/results/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ErrorTemplate, ErrorType
 from results.forms import SubmitErrorForm
+import re
+
 # Create your views here.
 def error_guide(error_name):
     html = "<div>"
@@ -40,13 +42,22 @@ def solve(request):
     """View function for presenting answers to the error trace."""
     error_trace = request.GET['error_trace']
     # NLTK CODE GOES HERE
+    no_punct = re.sub(r'[^\w\s]','',error_trace)
+    error_trace_list = no_punct.split()
+    error_type_list = []
+    for e in ErrorType.objects.all():
+        error_type_list.append(e.name)
+    #print(error_type_list)
+    main_error = list(set(error_trace_list).intersection(error_type_list))
+    print(main_error)
     # returns data into context
     num_templates = ErrorTemplate.objects.all().count()
     num_types = ErrorType.objects.all().count()
-    
+
     context = {
         'num_templates': num_templates,
         'num_types': num_types,
-        'error_trace': error_trace
+        'error_trace': error_trace,
+        'error_type': main_error[0],
     }
     return render(request, 'results.html', context=context)
