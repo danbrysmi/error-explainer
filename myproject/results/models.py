629241431e3903444eab78167c3c52391c42e877
Dan Bryan-Smith <danbrysmi@gmail.com>
2022-03-29 10:40:56 +0100

    Added models for Example and Tip

--- myproject/results/models.py
+++ myproject/results/models.py
@@ -21,3 +21,26 @@ class ErrorTemplate(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return self.template
+
+class Example(models.Model):
+    """Model representing code that is a worked example of an error, called in the Worked Examples slide."""
+    code_wrong = models.CharField(max_length=800, help_text='Incorrect code example.')
+    code_right = models.CharField(max_length=800, help_text='Correct code example.')
+    desc_wrong = models.CharField(max_length=800, help_text='Why does the incorrect code not work?')
+    desc_right = models.CharField(max_length=800, help_text='Why does the correct code work?')
+    tags = TaggableManager()
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.code_wrong
+
+class Tip(models.Model):
+    """Model representing a tip that is called in the Further Resources slide."""
+    name = models.CharField(max_length=50, help_text='Title of tip')
+    desc = models.CharField(max_length=800, help_text='Tip main text')
+    link = models.CharField(max_length=400, help_text='Link to resource')
+    tags = TaggableManager()
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f"{self.title} | {self.desc}"
